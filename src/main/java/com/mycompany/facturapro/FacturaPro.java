034423821893b77bae8d71958a748bc74d395b9e
Cristian Hernández <cristian.hernandezm750@gmail.com>
2025-11-29 12:34:27 -0500

    subimos todos los archivos a git

---
+++ src/main/java/com/mycompany/facturapro/FacturaPro.java
@@ -0,0 +1,13 @@
+package com.mycompany.facturapro;
+
+import com.mycompany.facturapro.GUI.Login;
+
+public class FacturaPro {
+
+    public static void main(String[] args) {
+        Login platform = new Login();
+        
+        platform.setVisible(true);
+        platform.setLocationRelativeTo(null);
+    }
+}
